80d2a426a6623f3b7e2bd051ce5f5d858239e827
twikgit <twikemailforstudying@gmail.com>
2024-12-23 00:50:08 +0300

    ?

--- BackendApi/obj/Debug/net8.0/BackendApi.AssemblyInfo.cs
+++ BackendApi/obj/Debug/net8.0/BackendApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5fecf63ed3cf7b5109e7b5ffd2d71a330f020cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+762520b40e365b36868e09af4756c369d22cb968")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
